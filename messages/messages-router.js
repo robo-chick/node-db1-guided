1d741c49dfade102a836ba57856e3add1f0cf7a9
Tasha <tashingsworth@gmail.com>
2020-08-18 21:36:45 -0400

    finished without final endpoints

--- messages/messages-router.js
+++ messages/messages-router.js
@@ -3,24 +3,71 @@ const db = require("../data/config")
 
 const router = express.Router()
 
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
+    try {
+        // translates to SELECT * FROM messages;
+        const messages = await db.select ("*").from("messages")
+
+        res.json(messages)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.get("/:id", async (req, res, next) => {
+    // translates to SELECT * FROM messages WHERE id = ?;
+    try {
+        const [message] = await db
+        .select("*")
+        .from("messages")
+        .where("id", req.params.id)
+        .limit(1)
+
+        res.json(message)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.post("/", async (req, res, next) => {
+    // translates to INSERT INTO messages (title, contents) VALUES (?, ?);
+    try {
+        const [id] = await db
+        .insert({
+            // database will automatically generate the ID
+            title: req.body.title,
+            contents: req.body.contents,
+        })
+        .into("messages")
+
+        // translates to SELECT * FROM messages WHERE id = ? LIMIT 1
+        const message = await db("messages")
+        .where("id", id)
+        .first 
+            
+        res.status(201).json(message)
+    } catch(err) {
+        next(err)
+    }
 
 })
 
-router.get("/:id", (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
+    // translates to UPDATE messages SET title = ? AND content = > WHERE id = ?
+    await db("messages")
+    .update({ /* new data */ })
+    .where("id", req.params.id)
 
-})
-
-router.post("/", (req, res, next) => {
-
-})
-
-router.put("/:id", (req, res, next) => {
-
+    // use req.params.id to make another SELECT statement to respond with
 })
 
 router.delete("/:id", (req, res, next) => {
+    // translates to DELETE FROM messages WHERE id = ?
+    await db("messages")
+        .where("id", req.params.id)
+        .del()
 
+        // send 204 back
 })
 
 module.exports = router
